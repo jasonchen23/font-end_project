5649b00cde2baf56e7bd639646310f2a49d39507
Jasonchen23 <kevin3121961@gmail.com>
2022-03-04 14:10:07 +0800

    function test success

--- db_connect.php
+++ db_connect.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname); //檢查連線
 if ($conn->connect_error) {
 die("連線失敗: " . $conn->connect_error); }
 else {
-    echo "連線成功";
+    // echo "連線成功";
 }
 // $conn->close();
 // 操作結束後關閉資料庫
